227c07d123e3daaf68a02388f6b13003dc3c1224
Ladrillo <gabriel@cabrejas.net>
2022-01-26 19:49:04 +0100

    committing to "lecture"

--- car/car.spec.js
+++ car/car.spec.js
@@ -63,8 +63,7 @@ describe('Car class', () => {
     expect(prius.odometer).toBe(0)
   })
   it('has a drive method that takes distance and inc odometer', () => {
-    // YOU
-    // no need for a new car
+    expect(prius.drive)
     // drive the car
     // assert that its odometer changed
   })
