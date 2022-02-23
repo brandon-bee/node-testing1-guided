07c2c64bcec7ab236757ace0961c1aef458f625a
Brandon B <64279000+brandon-bee@users.noreply.github.com>
2022-02-23 13:23:56 -0800

    GP Solution

--- car/car.js
+++ car/car.js
@@ -1,1 +1,28 @@
 // Build a Car class!
+function badCar() {
+  return {
+    model: 'Prius',
+    make: 'Toyota'
+  }
+}
+
+class Car {
+  constructor(make, model) {
+    this.make = make,
+    this.model = model,
+    this.odometer = 0
+  }
+  drive(miles) {
+    this.odometer += miles
+    return this.odometer
+  }
+  async driveAsync(miles) {
+    this.odometer += miles
+    return this.odometer
+  }
+}
+
+module.exports = {
+  badCar,
+  Car
+}
--- car/car.spec.js
+++ car/car.spec.js
@@ -1,1 +1,61 @@
 // Test away!
+const { badCar, Car } = require('./car')
+
+test('that it works', () => {
+  expect(true).toBe(true)
+})
+
+describe('badCar', () => {
+  it('returns a toyota', () => {
+    const actualValue = badCar()
+    const actualValue2 = actualValue
+    const expectedValue = {
+      make: 'Toyota',
+      model: 'Prius'
+    }
+
+    expect(actualValue).toEqual(expectedValue)
+    expect(actualValue).toBe(actualValue2)
+  })
+})
+
+describe('car class', () => {
+  let car
+  beforeEach(() => {
+    car = new Car('Toyota', 'Prius')
+  })
+
+  it('exists', () => {
+    expect(Car).toBeDefined()
+  })
+  it('can build instance of cars', () => {
+    const car = new Car()
+    expect(car).toBeInstanceOf(Car)
+  })
+  it('can build a car with make and model', () => {
+    expect(car).toHaveProperty('make')
+    expect(car).toHaveProperty('model')
+
+    expect(car.make).toBe('Toyota')
+    expect(car.model).toBe('Prius')
+
+    expect(car).toMatchObject({
+      model: 'Prius',make: 'Toyota'
+    })
+  })
+  it('new cars have an odometer prop initialized at zero', () => {
+    expect(car.odometer).toBe(0)
+  })
+  it('cars have a drive method', () => {
+    expect(car.drive).toBe(Car.prototype.drive)
+  })
+  it('driving the car adds miles to the odometer', () => {
+    car.drive(10)
+    expect(car.odometer).toBe(10)
+    car.drive(5)
+    expect(car.odometer).toBe(15)
+  })
+  it('driving ASYNC resolves to the updated odometer', async () => {
+    expect(await car.driveAsync(10)).toBe(10)
+  })
+})
